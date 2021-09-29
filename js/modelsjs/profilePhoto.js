f7921d9e47b9a16de51807cef13287cab0b172f8
UMUTBAYGUT <umutbaygut@hotmail.com>
2021-09-29 11:27:19 +0800

    imp materials

--- js/modelsjs/profilePhoto.js
+++ js/modelsjs/profilePhoto.js
@@ -4,6 +4,15 @@ import { test1 as UnrealBloomPass } from "../library/TransparentBackgroundFixedU
 console.log(UnrealBloomPass);
 
 //Start Imp To Bloom
+let bloomComposer, finalComposer;
+const materials = {};
+
+const params = {
+  exposure: 1,
+  bloomStrength: 5,
+  bloomThreshold: 0,
+  bloomRadius: 0,
+};
 const canvReference = document.getElementById("camerafeed");
 const renderer = new THREE.WebGLRenderer({
   canvas: canvReference,
@@ -13,10 +22,14 @@ renderer.setClearColor(0xff0000, 0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 const bloomPass = new UnrealBloomPass(
   new THREE.Vector2(window.innerWidth, window.innerHeight),
-  4,
-  1,
-  0.1
+  1.5,
+  0.4,
+  0.85
 );
+
+bloomPass.threshold = params.bloomThreshold;
+bloomPass.strength = params.bloomStrength;
+bloomPass.radius = params.bloomRadius;
 const composer = new THREE.EffectComposer(renderer);
 
 const loader = new THREE.GLTFLoader(); // This comes from GLTFLoader.js.
@@ -32,14 +45,56 @@ function animate() {
   requestAnimationFrame(animate);
   const delta = clock.getDelta();
   globalMixer.update(delta);
-  composer.render();
+  const darkMaterial = new THREE.MeshBasicMaterial({ color: "black" });
+  let count = 1;
+  profilePhotoModel.traverse((obj) => {
+    if (obj.isMesh && obj.name !== "buttonUP_rotating") {
+      materials[obj.uuid] = obj.material;
+      obj.material = darkMaterial;
+    }
+    count++;
+  });
+
+  bloomComposer.render();
+
+  profilePhotoModel.traverse((obj) => {
+    if (obj.isMesh) {
+      if (materials[obj.uuid]) {
+        obj.material = materials[obj.uuid];
+        delete materials[obj.uuid];
+      }
+    }
+  });
+
+  finalComposer.render();
 }
 
 export const profilePhotoModule = (group, rendererOld, camera, scene) => {
   //Start Bloom Effect
   const renderPass = new THREE.RenderPass(scene, camera);
-  composer.addPass(renderPass);
-  composer.addPass(bloomPass);
+
+  bloomComposer = new THREE.EffectComposer(renderer);
+  bloomComposer.renderToScreen = false;
+  bloomComposer.addPass(renderPass);
+  bloomComposer.addPass(bloomPass);
+
+  const finalPass = new THREE.ShaderPass(
+    new THREE.ShaderMaterial({
+      uniforms: {
+        baseTexture: { value: null },
+        bloomTexture: { value: bloomComposer.renderTarget2.texture },
+      },
+      vertexShader: document.getElementById("vertexshader").textContent,
+      fragmentShader: document.getElementById("fragmentshader").textContent,
+      defines: {},
+    }),
+    "baseTexture"
+  );
+  finalPass.needsSwap = true;
+
+  finalComposer = new THREE.EffectComposer(renderer);
+  finalComposer.addPass(renderPass);
+  finalComposer.addPass(finalPass);
 
   loader.load(
     profilePhotoModelFile,

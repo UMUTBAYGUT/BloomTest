2f6f0a2b246d91c254d9d94d29ec6d9159d4a906
connorgmeean <a@cgm.codes>
2021-09-30 20:06:47 +1000

    Re-implemented bloom

---
+++ js/appPipelineModule.js
@@ -0,0 +1,82 @@
+const profilePhotoModelFile = "./assets/models/Buttoncenterup.glb";
+
+const appPipelineModule = () => {
+  let scene, camera, renderer, clock, mixer;
+  const loader = new THREE.GLTFLoader();
+  return {
+    // Camera pipeline modules need a name. It can be whatever you want but must be
+    // unique within your app.
+    name: "app",
+
+    // onStart is called once when the camera feed begins. In this case, we need to wait for the
+    // XR8.Threejs scene to be ready before we can access it to add content. It was created in
+    // XR8.Threejs.pipelineModule()'s onStart method.
+    onStart: (args) => {
+      const { scene: _scene, camera: _camera, renderer: _renderer} = window.XR8.Threejs.xrScene();
+      scene = _scene;
+      camera = _camera;
+      renderer = _renderer;
+      clock = new THREE.Clock();
+
+      const axes = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1),
+        new THREE.MeshBasicMaterial({
+          color: 0xff0000,
+        })
+      );
+      // camera.add(axes);
+      axes.position.set(0, 0, -2);
+
+      loader.load(profilePhotoModelFile, (gltf) => {
+        const model = gltf.scene;
+        model.traverse((object) => {
+          if (object.isMesh) {
+            if (object.name === "buttonUP_pic_main") {
+              object.material.map = new THREE.TextureLoader().load(
+                "/assets/images2.jpg"
+              );
+              object.rotation.copy({
+                _x: 0,
+                _y: 0,
+                _z: 210.5,
+                _order: "XYZ",
+              });
+            }
+          }
+        });
+        mixer = new THREE.AnimationMixer(model);
+        const animation = gltf.animations[0];
+        const action = mixer.clipAction(animation);
+
+
+        action.play();
+
+        model.position.set(0, 0, -5);
+        model.rotation.copy({ _x: 0, _y: 0, _z: 0, _order: "XYZ" });
+        model.visible = true;
+        camera.add(model);
+      });
+    },
+    onDetach: () => {
+    },
+    onUpdate: ({ processCpuResult }) => {
+      const delta = clock.getDelta();
+      if (mixer) mixer.update(delta);
+    },
+    // Get a handle to the xr scene, camera and renderer. Returns:
+    // {
+    //   scene: The Threejs scene.
+    //   camera: The Threejs main camera.
+    //   renderer: The Threejs renderer.
+    // }
+    // Listeners are called right after the processing stage that fired them. This guarantees that
+    // updates can be applied at an appropriate synchronized point in the rendering cycle.
+    listeners: [
+      //  {event: 'reality.imagefound', process: showTarget},
+      // {event: 'reality.imageupdated', process: showTarget},
+      // {event: 'reality.imagelost', process: hideTarget},
+    ],
+  };
+};
+
+export default appPipelineModule;
---
+++ js/bloomPipelineModule.js
@@ -0,0 +1,192 @@
+const {Scene, PerspectiveCamera, WebGLRenderer, Vector2, MathUtils, DataTexture, RGBFormat, Color, WebGLRenderTarget, ReinhardToneMapping, ShaderPass, TexturePass, EffectComposer} = THREE;
+import { UnrealBloomPass } from './AlphaUnrealBloomPass.js';
+
+const bloomPipelineModule = () => {
+
+  let scene3;
+  let isSetup = false
+  let combinePass;
+  let bloomPass;
+  const cameraTextureCopyPosition = new Vector2(0, 0);
+  let cameraTexture;
+  let sceneTarget;
+  let copyPass;
+
+  console.log(document.getElementById);
+  const combineShaderFrag = document.getElementById("fragmentshader").textContent;
+  const combineShaderVert = document.getElementById("vertexshader").textContent;
+  const combineShader = {
+    uniforms: {
+      cameraTexture: { value: undefined },
+      tDiffuse: { value: null },
+      useAdditiveBlend: { value: false },
+    },
+    fragmentShader: combineShaderFrag, 
+    vertexShader: combineShaderVert,
+  }
+
+  const xrScene = () => {
+    return scene3;
+  }
+
+  const trySetup = ({canvas, canvasWidth, canvasHeight, GLctx}) => {
+    if (isSetup) {
+      return
+    }
+    isSetup = true;
+    const scene = new Scene();
+    scene.name = 'Root Scene';
+    const camera = new PerspectiveCamera(
+      60.0, /* initial field of view; will get set based on device info later. */
+      canvasWidth / canvasHeight,
+      0.01,
+      1000,
+    )
+    scene.add(camera)
+
+    const renderer = new WebGLRenderer({
+      canvas,
+      context: GLctx,
+      alpha: true,
+      antialias: true,
+    });
+    renderer.debug.checkShaderErrors = true;
+    renderer.autoClear = false;
+    renderer.autoClearDepth = false;
+    renderer.setClearColor(0xffffff, 0);
+    renderer.toneMapping = ReinhardToneMapping;
+    renderer.toneMappingExposure = 2.3;
+    renderer.setSize(canvasWidth, canvasHeight)
+
+    sceneTarget = new WebGLRenderTarget(canvasWidth, canvasHeight, { generateMipmaps: false });
+
+    // Bloom Composer
+    const bloomComposer = new EffectComposer(renderer);
+    bloomComposer.renderToScreen = false;
+
+    // Copy scene into bloom 
+    copyPass = new TexturePass(sceneTarget.texture);
+    bloomComposer.addPass(copyPass);
+
+    // Bloom Pass
+    bloomPass = new UnrealBloomPass(new Vector2(canvasWidth, canvasHeight), 2.2, 0, 0);
+    bloomPass.clearColor = new Color(0xffffff);
+    bloomPass.threshold = 0;
+    bloomComposer.addPass(bloomPass);
+
+    const gui = new dat.GUI({ autoPlace: true });
+    gui.add(bloomPass, "threshold", 0, 1, 0.01);
+    gui.add(bloomPass, "strength", 0, 3, 0.01);
+    gui.add(bloomPass, "radius", 0, 3, 0.01);
+    gui.add(renderer, "toneMappingExposure", 0, 5, 0.01);
+    gui.domElement.style.zIndex = "9999";
+    console.log(gui);
+
+
+    // Final composer
+    const composer = new EffectComposer( renderer );
+    composer.addPass(copyPass);
+
+    // Combine scene and camerafeed pass
+    combinePass = new ShaderPass(combineShader)
+    combinePass.clear = false;
+    combinePass.renderToScreen = true;
+    composer.addPass(combinePass);
+
+    scene3 = { scene, camera, renderer, bloomComposer, composer }
+    // @ts-expect-error no type definition
+    window.scene3 = scene3;
+    // Overwrite the default threejs getter in 8thwall so that you can get the bloomComposer component as well
+    window.XR8.Threejs.xrScene = xrScene;
+  }
+
+  return {
+    name: 'customthreejs',
+    onStart: (args) => trySetup(args),
+    onDetach: () => { isSetup = false },
+    onUpdate: ({processCpuResult}) => {
+      const realitySource = processCpuResult.reality || processCpuResult.facecontroller
+
+      if (!realitySource) {
+        return
+      }
+
+      const {rotation, position, intrinsics} = realitySource
+      const {camera} = scene3
+
+      for (let i = 0; i < 16; i++) {
+        camera.projectionMatrix.elements[i] = intrinsics[i]
+      }
+
+      // Fix for broken raycasting in r103 and higher. Related to:
+      //   https://github.com/mrdoob/three.js/pull/15996
+      // Note: camera.projectionMatrixInverse wasn't introduced until r96 so check before setting
+      // the inverse
+      if (camera.projectionMatrixInverse) {
+        camera.projectionMatrixInverse.copy(camera.projectionMatrix).invert()
+      }
+
+      if (rotation) {
+        camera.setRotationFromQuaternion(rotation)
+      }
+      if (position) {
+        camera.position.set(position.x, position.y, position.z)
+      }
+    },
+    onCanvasSizeChange: ({canvasWidth, canvasHeight, videoWidth, videoHeight}) => {
+      if (!isSetup) {
+        return
+      }
+      cameraTexture = new DataTexture(new Uint8Array(canvasWidth * canvasHeight * 3), canvasWidth, canvasHeight, RGBFormat);
+
+      const {renderer} = scene3
+      renderer.setSize(canvasWidth, canvasHeight)
+      const pixelRatio = MathUtils.clamp(window.devicePixelRatio, 1, 2);
+      renderer.pixelRatio = pixelRatio;
+      //
+      // Update render pass sizes
+      scene3.bloomComposer.setSize(canvasWidth * pixelRatio, canvasHeight * pixelRatio);
+      scene3.bloomComposer.passes.forEach(pass => {
+        if (pass.setSize) {
+          pass.setSize(canvasWidth * pixelRatio, canvasHeight * pixelRatio);
+        }
+      })
+      scene3.composer.setSize(canvasWidth * pixelRatio, canvasHeight * pixelRatio);
+      scene3.composer.passes.forEach(pass => {
+        if (pass.setSize) {
+          pass.setSize(canvasWidth * pixelRatio, canvasHeight * pixelRatio);
+        }
+      })
+      if (bloomPass && combinePass && sceneTarget && copyPass) {
+        combinePass.uniforms['cameraTexture'] = { value: cameraTexture };
+        combinePass.uniforms.bloomTexture = { value: bloomPass.renderTargetsHorizontal[0] };
+        sceneTarget.setSize(canvasWidth * pixelRatio, canvasHeight * pixelRatio);
+        copyPass.uniforms['tDiffuse'] = { value: sceneTarget.texture };
+      }
+    },
+    onRender: () => {
+      // renderer.render(scene, camera)
+      if (cameraTexture) {
+        scene3.renderer.copyFramebufferToTexture(cameraTextureCopyPosition, cameraTexture)
+      }
+      if (sceneTarget) {
+        scene3.renderer.setRenderTarget(sceneTarget);
+      }
+      scene3.renderer.clear();
+      scene3.renderer.clearDepth();
+      scene3.renderer.render(scene3.scene, scene3.camera);
+      scene3.renderer.setRenderTarget(null);
+      scene3.bloomComposer.render();
+      scene3.composer.render();
+    },
+    // Get a handle to the xr scene, camera and renderer. Returns:
+    // {
+    //   scene: The Threejs scene.
+    //   camera: The Threejs main camera.
+    //   renderer: The Threejs renderer.
+    // }
+    xrScene,
+  }
+}
+
+export default bloomPipelineModule;
